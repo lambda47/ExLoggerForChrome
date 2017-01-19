96a4a578ab5f33d10d0429a0a7fb58600cdb5dce
Lambda47 <liwei8747@163.com>
2017-01-19 09:12:01 +0800

    Browser Actions增加开关功能

--- js/background.js
+++ js/background.js
@@ -1,18 +1,42 @@
 (function() {
     var responseQuene = [];
+
+    if (localStorage.enabled == null) {
+        localStorage.enabled = 'y';
+    }
+
+    function displayIcon() {
+        if (localStorage.enabled === 'y') {
+            chrome.browserAction.setIcon({path: '../icon/icon_48.png'});
+        } else {
+            chrome.browserAction.setIcon({path: '../icon/icon_48_black.png'});
+        }
+    }
+
+    displayIcon();
+
     chrome.webRequest.onCompleted.addListener(function(details) {
-        chrome.tabs.sendMessage(details.tabId, {details: details}, function(response) {
-            if (!response) {
-                responseQuene.push(details);
-            }
-        });
+        if (localStorage.enabled === 'y') {
+            chrome.tabs.sendMessage(details.tabId, {details: details}, function (response) {
+                if (!response) {
+                    responseQuene.push(details);
+                }
+            });
+        }
     }, {urls: ["<all_urls>"]}, ["responseHeaders"]);
 
     chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
         if (request === 'ready') {
-            sendResponse(responseQuene);
+            if (localStorage.enabled === 'y') {
+                sendResponse(responseQuene);
+            }
             responseQuene = [];
         }
+    });
+
+    chrome.browserAction.onClicked.addListener(function(tab) {
+        localStorage.enabled = localStorage.enabled === 'y' ? 'n' : 'y';
+        displayIcon();
     });
 })();
 
